58e4c6a338c8b25caa74565cfb1bd5ebae1933d9
Divya Mittal <divya.mittal@innoraft.com>
2025-04-07 10:22:47 +0530

    Fres-101:Task2 completed.

--- form.php
+++ form.php
@@ -36,8 +36,38 @@ class Form {
     $data = htmlspecialchars($data);
     return $data;
   }
+  public function imageValidation() {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if (isset($_FILES['user_image']) && $_FILES['user_image']['error'] === 0) {
+          $target_dir = "uploads/";  
+          if (!is_dir($target_dir)) {
+              mkdir($target_dir, 0777, true);  
+          }
+  
+          $target_file = $target_dir . basename($_FILES["user_image"]["name"]);
+          $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+  
+          
+          
+          $allowed_extensions = ['jpg', 'jpeg', 'png', 'gif'];
+          if (!in_array($imageFileType, $allowed_extensions)) {
+              echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+          } else {
+              if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $target_file)) {
+                echo "<img width='300' height='300' src='$target_file' /><br>";
+                  echo"<h2>$this->fullname</h2>";
+              } else {
+                  echo "Error moving file to uploads directory.";
+              }
+          }
+      } else {
+          echo "No file uploaded or an error occurred.";
+      }
+    }
+  }
 }
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $formdata = new Form();
+    $formdata->imageValidation();
   }
 ?>
